7c8511f70164dbe3a6a87d79f426db5cbffe3b0b
khouloud711 <bejaouikhouloud98@gmail.com>
2022-07-26 16:56:16 +0200

    get categ

--- src/main/java/com/server/server/Service/CategoryService.java
+++ src/main/java/com/server/server/Service/CategoryService.java
@@ -5,17 +5,36 @@ import com.server.server.Entity.Product;
 import com.server.server.Repository.CategoryRepository;
 import com.server.server.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
     @Autowired
     CategoryRepository categoryRepository;
+
     public List<Category> findAll() {
 
         return categoryRepository.findAll();
+    }
+
+    //get all categs
+    public List<Category> getCategory() {
+        return categoryRepository.findAll();
+    }
+    //get by id
+    public ResponseEntity<Category> getCategory(long id) {
+
+        Optional<Category> optionalCategory = categoryRepository.findById(id);
+        if (optionalCategory.isPresent()) {
+            return ResponseEntity.ok(optionalCategory.get());
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
