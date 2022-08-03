38c2cbf3d60ff5864df2ae0723d611a8f00b4e02
khouloud711 <bejaouikhouloud98@gmail.com>
2022-08-03 15:21:08 +0200

    get images by product id

--- src/main/java/com/server/server/Repository/ImagesRepository.java
+++ src/main/java/com/server/server/Repository/ImagesRepository.java
@@ -1,11 +1,14 @@
 package com.server.server.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.server.server.Entity.Images;
+import com.server.server.Entity.Product;
 
 @Repository
 public interface ImagesRepository extends JpaRepository<Images,Long> {
-
+  public List<Images> findAllByProductId(long id_product);
 }
--- src/main/java/com/server/server/Service/ImagesService.java
+++ src/main/java/com/server/server/Service/ImagesService.java
@@ -1,8 +1,27 @@
 package com.server.server.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.server.server.Entity.Images;
+import com.server.server.Entity.Product;
+import com.server.server.Repository.ImagesRepository;
+import com.server.server.Repository.ProductRepository;
+import com.server.server.Repository.PromotionRepository;
 
 @Service
 public class ImagesService {
+
+  @Autowired
+  ImagesRepository imagesRepository;
+
+  // get images by id-product
+  public List<Images> findImagesByProduct_Id(long id_product) {
+
+    return imagesRepository.findAllByProductId(id_product);
+
+  }
 
 }
