77fb56a36a0a0ae663becdc575a4fe8582516c43
Amdouni-Dev <amdouni.mouna@esprit.tn>
2022-08-07 22:15:18 +0100

    Commit4(Front-End):Fix Nav

---
+++ src/main/java/com/server/server/Controller/AdminController/IndexCtrl.java
@@ -0,0 +1,45 @@
+package com.server.server.Controller.AdminController;
+
+import javax.validation.Valid;
+
+import com.server.server.Entity.Employee;
+import com.server.server.Entity.Product;
+import com.server.server.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+import java.util.List;
+import com.server.server.Service.ProductService;
+@RequestMapping("/oauth")
+@Controller
+public class IndexCtrl {
+	@Autowired
+	private ProductService productService;
+	@RequestMapping("/login")
+	public String loginPage(){
+		return "auth-login";
+	}
+
+	@RequestMapping("/home")
+	public String loginSubmit(){
+		return "/layouts/dashboard";
+	}
+
+
+
+
+
+	@RequestMapping("/showProducts")
+	public String viewHomePagdllkdkldk(Model model) {
+		List<Product> products=productService.findAll();
+		model.addAttribute("list",products);
+		return "/layouts/produits";
+	}
+}
