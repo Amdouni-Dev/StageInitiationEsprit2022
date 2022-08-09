162484a590b868d375bb2bc7a1245817c25ac3d7
19hes96 <96926724+19hes96@users.noreply.github.com>
2022-08-09 05:31:05 +0100

    09.08.22

--- src/main/java/com/server/server/Controller/AdminController/IndexCtrl.java
+++ src/main/java/com/server/server/Controller/AdminController/IndexCtrl.java
@@ -1,21 +1,11 @@
 package com.server.server.Controller.AdminController;
 
-import javax.validation.Valid;
 
-import com.server.server.Entity.Employee;
-import com.server.server.Entity.Product;
-import com.server.server.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
-import java.util.List;
 import com.server.server.Service.ProductService;
 @RequestMapping("/oauth")
 @Controller
@@ -29,7 +19,7 @@ public class IndexCtrl {
 
 	@RequestMapping("/home")
 	public String loginSubmit(){
-		return "/layouts/dashboard";
+		return "/pages/landing_page";
 	}
 
 }
